61575ae9e76da3caca652511868a6d1deac99287
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-07-29 21:33:03 +0500

    Custom Welcome Message
    i added custom welcome message according to my requirements :)

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -31,6 +31,15 @@ async def new_join_f(client, message):
     # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
 
+    
+async def welcome_message_f(client, message):
+    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    #channel_id = str(AUTH_CHANNEL)[4:]
+    #message_id = 99
+    # display the /help
+    
+    await message.reply_text("""Welcome to Team Drive Ultimate X""", disable_web_page_preview=True)    
+    
 
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
